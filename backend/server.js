42a8425c22c99985d7b4d6f6ed244d1ab93b9e94
hop <avinashseelall@gmail.com>
2023-01-21 18:25:15 -0500

    learning more sockets

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,39 @@
-const io = require("socket.io")(3001,
+const express = require("express")
+const app = express()
+const http = require("http")
+const {Server} = require("socket.io")
+const cors = require("cors")
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+
+
+
+const io = new Server(server,
     {
         cors: {
             origin: ["http://localhost:3000"],
         }
     })
 
+server.listen(3001, ()=> {
+    console.log("server running")
+})
 io.on('connection', socket => {
     console.log(socket.id)
-    socket.on("updatedName", (name) => {
-        console.log(name)
+    
+    socket.on("send_message", (data) => {
+        console.log(data)
+        socket.broadcast.emit("receive_message", data)
+        for (let i = 0; i < 300; i++) {
+            socket.broadcast.emit("receive_n", {n: Math.random()})
+
+        }
     })
+
+
+
+
 })
