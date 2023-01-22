c3b831bbbe88ed3b1b0c08ed851327249ee24c2e
hop <avinashseelall@gmail.com>
2023-01-21 20:38:54 -0500

    deleted these by accident :(

---
+++ frontend/src/components/lobby.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./lobby"
+import Chat from './chat'
+
+function Lobby() {
+  return (
+    <div className='lob'> 
+    
+    </div>
+  )
+}
+
+export default Lobby
